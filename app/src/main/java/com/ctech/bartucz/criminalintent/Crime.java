0ed3185e9b163a08f6be5e4573db1eb0a3934b54
Mr. Bartucz <jobartucz@isd535.org>
2019-03-31 22:24:51 -0500

    Added Crime class

---
+++ app/src/main/java/com/ctech/bartucz/criminalintent/Crime.java
@@ -0,0 +1,45 @@
+package com.ctech.bartucz.criminalintent;
+
+import java.util.Date;
+import java.util.UUID;
+
+public class Crime {
+
+    private UUID mId;       // the crime's unique ID
+    private String mTitle;  // title of the crime
+    private Date mDate;     // date of the crime
+    private boolean mSolved; // has the crime been solved?
+
+    public Crime() {
+        mId = UUID.randomUUID(); // automatically create a guaranteed-unique ID.
+        mDate = new Date();      // default the crime date to right now
+    }
+
+    public UUID getId() {
+        return mId;
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate(Date date) {
+        mDate = date;
+    }
+
+    public boolean isSolved() {
+        return mSolved;
+    }
+
+    public void setSolved(boolean solved) {
+        mSolved = solved;
+    }
+}
