95e44489880428b4d51a063f97d6cd2058804799
Mr. Bartucz <jobartucz@isd535.org>
2019-04-06 20:54:58 -0500

    made MainActivity a subclass of SingleFragmentActivity

--- app/src/main/java/com/ctech/bartucz/criminalintent/MainActivity.java
+++ app/src/main/java/com/ctech/bartucz/criminalintent/MainActivity.java
@@ -1,26 +1,11 @@
 package com.ctech.bartucz.criminalintent;
 
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends SingleFragmentActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        FragmentManager fm = getSupportFragmentManager();
-        Fragment myFragment = fm.findFragmentById(R.id.fragment_container);
-
-        // the first time through, the fragment will be null, so create it
-        if (myFragment == null) {
-            myFragment = new CrimeFragment();
-            fm.beginTransaction()
-                    .add(R.id.fragment_container, myFragment)
-                    .commit();
-        }
+    protected Fragment createFragment() {
+        return new CrimeFragment();
     }
 }
