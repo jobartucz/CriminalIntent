5cb28c4659fd1f1667e5982b2188ba9c8361abd4
Mr. Bartucz <jobartucz@isd535.org>
2019-04-06 20:30:12 -0500

    Added the new SingleFragmentActivity abstract superclass

---
+++ app/src/main/java/com/ctech/bartucz/criminalintent/SingleFragmentActivity.java
@@ -0,0 +1,30 @@
+package com.ctech.bartucz.criminalintent;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+
+public abstract class SingleFragmentActivity extends AppCompatActivity {
+
+    protected abstract Fragment createFragment();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FragmentManager fm = getSupportFragmentManager();
+        Fragment myFragment = fm.findFragmentById(R.id.fragment_container);
+
+        // the first time through, the fragment will be null, so create it
+        if (myFragment == null) {
+            myFragment = createFragment();
+            fm.beginTransaction()
+                    .add(R.id.fragment_container, myFragment)
+                    .commit();
+        }
+    }
+
+}
+
