fbbb09d0d01391e257b5ea0e08c95444c9a076fa
Mr. Bartucz <jobartucz@isd535.org>
2019-04-06 20:30:45 -0500

    Just changed the name of a variable back to the way it was.

--- app/src/main/java/com/ctech/bartucz/criminalintent/MainActivity.java
+++ app/src/main/java/com/ctech/bartucz/criminalintent/MainActivity.java
@@ -13,13 +13,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         FragmentManager fm = getSupportFragmentManager();
-        Fragment crimeListFragment = fm.findFragmentById(R.id.fragment_container);
+        Fragment myFragment = fm.findFragmentById(R.id.fragment_container);
 
         // the first time through, the fragment will be null, so create it
-        if (crimeListFragment == null) {
-            crimeListFragment = new CrimeFragment();
+        if (myFragment == null) {
+            myFragment = new CrimeFragment();
             fm.beginTransaction()
-                    .add(R.id.fragment_container, crimeListFragment)
+                    .add(R.id.fragment_container, myFragment)
                     .commit();
         }
     }
