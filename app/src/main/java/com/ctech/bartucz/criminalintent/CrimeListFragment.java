0a1749b384a510de5249f2e2746af551eddfa251
Mr. Bartucz <jobartucz@isd535.org>
2019-04-08 22:36:19 -0500

    Added the CrimeListHolder Added list_item_crime layout as well

--- app/src/main/java/com/ctech/bartucz/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/ctech/bartucz/criminalintent/CrimeListFragment.java
@@ -1,9 +1,36 @@
 package com.ctech.bartucz.criminalintent;
 
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.text.Layout;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 public class CrimeListFragment extends Fragment {
 
-    // don't put anything here yet
+    private RecyclerView mCrimeRecyclerView;
 
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+
+        View myView = inflater.inflate(R.layout.fragment_crime_list, container, false);
+
+        mCrimeRecyclerView = myView.findViewById(R.id.crime_recycler_view);
+        mCrimeRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        return myView;
+    }
+
+    private class CrimeHolder extends RecyclerView.ViewHolder {
+        public CrimeHolder(LayoutInflater inflater, ViewGroup parent) {
+            super(inflater.inflate(R.layout.list_item_crime, parent, false));
+        }
+    }
 }
+
